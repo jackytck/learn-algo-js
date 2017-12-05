945709ffac49e8624aed287249e067a58aef4ca4
Jacky Tang <jackytck@gmail.com>
2017-12-06 00:06:48 +0800

    :sparkles: maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,23 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar (str) {
+  const charMap = {}
+  let max = 0
+  let maxChar = ''
 
-module.exports = maxChar;
+  for (let c of str) {
+    charMap[c] = charMap[c] + 1 || 1
+  }
+
+  for (let c in charMap) {
+    if (charMap[c] > max) {
+      max = charMap[c]
+      maxChar = c
+    }
+  }
+
+  return maxChar
+}
+
+module.exports = maxChar
