79163b8173b2bff17dc7d0b72fdca997d21b77a5
Jacky Tang <jackytck@gmail.com>
2017-12-12 00:06:24 +0800

    :sparkles: pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,39 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid (n) {
+  const width = 2 * n - 1
+  const midPoint = Math.floor(width / 2)
+  for (let i = 0; i < n; i++) {
+    let level = ''
 
-module.exports = pyramid;
+    for (let j = 0; j < width; j++) {
+      if (midPoint - i <= j && j <= midPoint + i) {
+        level += '#'
+      } else {
+        level += ' '
+      }
+    }
+    console.log(level)
+  }
+}
+
+function pyramid2 (n, row = 0, level = '') {
+  if (row === n) {
+    return
+  }
+
+  if (level.length === 2 * n - 1) {
+    console.log(level)
+    return pyramid2(n, row + 1)
+  }
+
+  const midPoint = Math.floor((2 * n - 1) / 2)
+  let add = ' '
+  if (midPoint - row <= level.length && level.length <= midPoint + row) {
+    add = '#'
+  }
+  pyramid2(n, row, level + add)
+}
+
+module.exports = pyramid
