a7d1d5fdeeee46a29600265fff361caab56f3ad2
Jacky Tang <jackytck@gmail.com>
2017-12-22 00:11:28 +0800

    :sparkles: LinkedList: constructor + insertFirst + size

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -9,6 +9,24 @@ class Node {
   }
 }
 
-class LinkedList {}
+class LinkedList {
+  constructor () {
+    this.head = null
+  }
+
+  insertFirst (data) {
+    this.head = new Node(data, this.head)
+  }
+
+  size () {
+    let counter = 0
+    let node = this.head
+    while (node) {
+      counter++
+      node = node.next
+    }
+    return counter
+  }
+}
 
 module.exports = { Node, LinkedList }
